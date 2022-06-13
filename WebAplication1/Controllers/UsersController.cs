430b67cf438f712202249f67a3621e1b0d41b19d
OneStep55 <vnmcfn@gmail.com>
2022-06-13 22:25:39 +0600

    init project

---
+++ WebAplication1/Controllers/UsersController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using WebAplication1.Data;
+using WebAplication1.Models;
+
+namespace WebAplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable <UserModel> users = _db.Users;
+            return View(users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(UserModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "User created successfully";
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDB = _db.Users.Find(id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(UserModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "User edited successfully";
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDB = _db.Users.Find(id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var user = _db.Users.Find(id);
+            if (user == null)
+                return NotFound();
+            
+            _db.Remove(user);
+            _db.SaveChanges();
+            TempData["success"] = "User deleted successfully";
+            return RedirectToAction("Index");
+            
+        }
+
+    }
+}
---
+++ WebAplication1/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAplication1.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Age { get; set; }
+    }
+}
